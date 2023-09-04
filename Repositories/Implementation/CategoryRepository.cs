3b3a477fc08c722ef78af7bf7da5cee52b53c451
Zhekai Dong <85309696+zdong2080@users.noreply.github.com>
2023-09-04 13:50:43 -0500

    Add files via upload

---
+++ Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using NetBlog.Back.Data;
+using NetBlog.Back.Models.Domain;
+using NetBlog.Back.Repositories.Interface;
+
+namespace NetBlog.Back.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
---
+++ Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using NetBlog.Back.Models.Domain;
+
+namespace NetBlog.Back.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        
+        Task<Category> CreateAsync(Category category);
+    }
+}
