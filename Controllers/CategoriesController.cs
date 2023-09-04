258f52d9df3a771e81a3b0004a203ad481f585d2
Zhekai Dong <85309696+zdong2080@users.noreply.github.com>
2023-09-04 13:51:31 -0500

    Add files via upload

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetBlog.Back.Data;
+using NetBlog.Back.Models.Domain;
+using NetBlog.Back.Models.DTO;
+using NetBlog.Back.Repositories.Interface;
+
+namespace NetBlog.Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            // Map Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+
+            return Ok(response);
+        }
+    }
+}
