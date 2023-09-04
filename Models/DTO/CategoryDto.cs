05adc972af2a08a9cc0c8594f4a2cc1e01158cf7
Zhekai Dong <85309696+zdong2080@users.noreply.github.com>
2023-09-04 13:51:15 -0500

    Add files via upload

---
+++ Models/DTO/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NetBlog.Back.Models.DTO
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
