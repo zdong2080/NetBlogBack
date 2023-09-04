19e348b14cf5cfd6f32f2b1fd6e3f45df54978d8
Zhekai Dong <85309696+zdong2080@users.noreply.github.com>
2023-09-04 13:50:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using NetBlog.Back.Data;
+using NetBlog.Back.Repositories.Implementation;
+using NetBlog.Back.Repositories.Interface;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NetBlogConnectionString"));
+});
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
